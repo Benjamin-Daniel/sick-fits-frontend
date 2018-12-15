894e2fbf954e3fadfd57a96e83dbe8468dded947
Benjamin-Daniel <benjamindaniel706@gmail.com>
2018-12-15 13:34:23 +0100

    displaying single order

--- components/TakeMyMoney.js
+++ components/TakeMyMoney.js
@@ -29,12 +29,16 @@ function totalItems(cart) {
 
 class TakeMyMoney extends React.Component {
     onToken = async (res, createOrder) => {
+        Nprogress.start();
         const order = await createOrder({
             variables: {
                 token: res.id,
             },
         }).catch(err => alert(err.message));
-        console.log(order);
+        Router.push({
+            pathname: '/order',
+            query: { id: order.data.createOrder.id },
+        })
     }
     render() {
         return (
